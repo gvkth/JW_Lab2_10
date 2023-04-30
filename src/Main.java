07ee39052337709f853266599085f8410dace1ed
cuongvk <cuongvk.hcm@vnpt.vn>
2023-04-30 23:08:30 +0700

    Start Lab2.10

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        //load the spring configuration file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Coach myCoach = context.getBean("myCricketCoach",CricketCoach.class);
+
+        System.out.println(myCoach.getDailyWorkout());
+        System.out.println(myCoach.getDailyFortune());
+        context.close();
+
+    }
+}
